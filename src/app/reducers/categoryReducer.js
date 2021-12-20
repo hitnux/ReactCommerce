1a1d298b06f7b36f9f47ebfb650bce32cd28925d
Halil Bilgin <hlilbilgin@gmail.com>
2021-12-20 18:28:51 +0300

    Update categoryReducer.js

--- src/app/reducers/categoryReducer.js
+++ src/app/reducers/categoryReducer.js
@@ -4,13 +4,13 @@ import { getCategories } from '../services/Api'
 export const categoryReducer = createSlice({
     name: 'categories',
     initialState: {
-        list: getCategories()
+        list:  getCategories()
     },
     reducers: {
     },
 })
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = categoryReducer.actions
+//export const { increment, decrement, incrementByAmount } = categoryReducer.actions
 
 export default categoryReducer.reducer
